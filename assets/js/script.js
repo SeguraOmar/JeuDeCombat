6d8e796a27e254845a2ea803683aca5cab87b470
Omar <omarsegura76300@gmail.com>
2023-11-06 16:02:21 +0100

    ajout JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,8 @@
-function aleatoire(min, max) {
-
-    return Math.floor(Math.round(max - min) * Math.random() + min);
-
-}
-
-class character {
+class Character {
     constructor(_nom, _vie, _attaque, _defense) {
-
         this.existe = false;
 
-        if (_nom != "") {
+        if (_nom !== "") {
             this.nom = _nom;
             this.vie = _vie;
             this.attaque = _attaque;
@@ -18,52 +11,71 @@ class character {
         } else {
             this.nom = "error";
         }
-
-
     }
 
     info() {
-        console.log(this.nom + ": vie: " + this.vie + " - attaque: " + this.attaque + " -defense: " + this.defense);
-
+        console.log(this.nom + ": vie: " + this.vie + " - attaque: " + this.attaque + " - défense: " + this.defense);
     }
 
     attaquer(_defenseur) {
-        console.log("attaque de" + this.nom + "sur" + _defenseur.nom)
+        console.log("attaque de " + this.nom + " sur " + _defenseur.nom);
 
         if (this.attaque > _defenseur.defense) {
-            _defenseur.vie -= 10
-
-        }
-        else if (this.attaque == _defenseur.defense) {
-
-            _defenseur.vie -= 5
-        }
-        else {
-            this.vie -= 5
+            _defenseur.vie -= 10;
+        } else if (this.attaque === _defenseur.defense) {
+            _defenseur.vie -= 5;
+        } else {
+            this.vie -= 5;
         }
 
         if (this.vie <= 0) {
-            console.error(this.nom + "est mort")
-            this.existe = false
-        }
-
-        else if (_defenseur.vie <= 0) {
-            console.error(_defenseur.nom + "est mort")
-            this.existe = false
+            console.error(this.nom + " est mort");
+            this.existe = false;
+        } else if (_defenseur.vie <= 0) {
+            console.error(_defenseur.nom + " est mort");
+            this.existe = false;
         }
     }
-};
 
-let perso1 = new character("CRS", aleatoire(20, 100), aleatoire(20, 100), aleatoire(20, 100));
-let perso2 = new character("GJ", aleatoire(20, 100), aleatoire(20, 100), aleatoire(20, 100))
+    static aleatoire(min, max) {
+        return Math.floor(Math.round(max - min) * Math.random() + min);
+    }
+}
 
-perso1.info()
+const nbrJoueur = 2;
+const players = [];
 
-perso2.info()
+for (let i = 0; i < nbrJoueur; i++) {
+    const nom = prompt("Veuillez saisir un nom");
+    const vie = Character.aleatoire(20, 100);
+    const attaque = Character.aleatoire(20, 100);
+    const defense = Character.aleatoire(20, 100);
+    
+    const player = new Character(nom, vie, attaque, defense);
+    player.info();
+    
+    players.push(player);
+}
 
-perso1.attaquer(perso2)
+let attaquant
+let defenseur
 
-perso1.info()
+function run () {
 
-perso2.info()
+}
+
+
+// let perso1 = new character("CRS", character.aleatoire(20, 100), character.aleatoire(20, 100), character.aleatoire(20, 100));
+// let perso2 = new character("GJ", character.aleatoire(20, 100), character.aleatoire(20, 100), character.aleatoire(20, 100))
+
+// perso1.info()
+
+// perso2.info()
+
+// perso1.attaquer(perso2)
+
+// perso1.info()
+
+// perso2.info()
+
 
